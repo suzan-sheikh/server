d298dc890934d7d2f23e0dede3415325c2734b02
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-12 21:26:31 +0600

    module one complite

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -11,19 +11,19 @@ export const productController = (
   const urlParts = url?.split("/");
   const id = urlParts && urlParts[1] === "product" ? Number(urlParts[2]) : null;
 
-
-
-
   if (url === "/product" && method === "GET") {
     // const product = [{ id: 12, name: "suzan", company: "software" }];
     const products = readProduct();
     res.writeHead(200, { "content-type": "application/json" });
     res.end(
-      JSON.stringify({ message: "product sent successful", data: products }),
+      JSON.stringify({ message: "products sent successful", data: products }),
     );
   } else if (method === "GET" && id !== null) {
     const products = readProduct();
     const product = products.find((p: IProduct) => p.id === id);
-    console.log(product);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({ message: "product sent successful", data: product }),
+    );
   }
 };
--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -6,6 +6,5 @@ const filePath = path.join(process.cwd(), "src/database/db.json");
 export const readProduct = () => {
   // console.log(filePath);
   const product = fs.readFileSync(filePath, "utf-8");
-  console.log(product);
   return JSON.parse(product);
 };
